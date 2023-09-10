cebf4f61933feb3f4b67bab4d72b233de0e538e0
Nor-Eddin <noreddinlam@gmail.com>
2023-09-10 21:14:27 +0200

    mise en place des routes  du site

--- webapi/Models/Promotions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace webapi.Models
-{
-    public class Promotions
-    {
-        public int SaveOff { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; } = DateTime.MinValue;
-    }
-}
